1abf731b719a6091ca069a7951b5d6b3e6f1fe79
haihire <86287922+haihire@users.noreply.github.com>
2025-10-22 09:11:43 +0900

    Clean up SevUrl.js by removing commented code
    Removed commented-out code for test and services configurations.

--- SevUrl.js
+++ SevUrl.js
@@ -16,37 +16,8 @@ export function SevUrl() {
   const vShotPad = "baccarat-pad.pd-bet-services.com";
   const vShotPad_ctrl = "baccarat-control.pd-bet-services.com";
 
-  //test
-  //  const url = "baccarat-server.pd-bet-test.com";
-  // const CallBack = "so-test";
-  // const server_port = 7771;
-  // const scanner_port = 8806;
-  // const express_port = 80;
-  // const sql_host = "172.31.38.103";
-  // const sql_user = "holdem";
-  // const sql_password = "holdem!@12";
-  // const allowUrl = "services";
 
-  // const vShotDealer = "baccarat-dealer.pd-bet-test.com";
-  // const vShotspd_bt = "pd-bet-test.com";
-  // const vShotspdbt = "spdbt.com";
-  // const vShotPad = "baccarat-pad.pd-bet-test.com";
-
-  //services
-  //  const url = "baccarat-server.pd-bet-services.com";
-  // const CallBack = "so-api";
-  // const server_port = 7771;
-  // const scanner_port = 8806;
-  // const express_port = 80;
-  // const sql_host = "172.31.16.196";
-  // const sql_user = "holdem";
-  // const sql_password = "holdem!@12";
-  // const allowUrl = "services";
-
-  // const vShotDealer = "baccarat-dealer.pd-bet-services.com";
-  // const vShotspd_bt = "0857-services.com";
-  // const vShotspdbt = "spdbt.com";
-  // const vShotPad = "baccarat-pad.pd-bet-services.com";
+  
 
   return {
     CallBack: CallBack,
